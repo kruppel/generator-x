1b4e5f72db0ab5ed7b8ba3a00496f0e3573c7c88
Kurt Ruppel <kruppel@zendesk.com>
2014-06-05 09:31:43 -0700

    Use path.join to pass static dir names.

--- app/templates/lib/server.js
+++ app/templates/lib/server.js
@@ -1,11 +1,14 @@
+'use strict';
+var path = require('path');
+
 module.exports = {
   listen: function(callback) {
     var express = require('express'),
         app = express();
 
     app.use(require('connect-livereload')());
-    app.use(express.static(__dirname + '/../app'));
-    app.use(express.static(__dirname + '/../tmp'));
+    app.use(express.static(path.join(__dirname, '..', 'app')));
+    app.use(express.static(path.join(__dirname, '..', 'tmp')));
 
     app.listen(process.env.PORT || 3000, callback);
   }
